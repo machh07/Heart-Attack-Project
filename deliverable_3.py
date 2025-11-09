389963a119528ce9edf8c2a799e0b67f2dc7e038
machh07 <marieangechhuon@gmail.com>
2025-11-08 21:48:26 -0500

    Start manipulation of dataset
    Started broadly analyzing dataset using functions + made sure no duplicated nor empty values.

--- deliverable_3.py
+++ deliverable_3.py
@@ -8,3 +8,13 @@ import seaborn as sns
 
 #Import dataset
 df = pd.read_csv("Medicaldataset.csv")
+
+#Preliminary steps
+#Initial data inspection
+print(df.info())
+print(df.describe())
+#Handle duplicate entries
+print(df.duplicated().value_counts()) #There are no duplicates in this dataset 
+#Identify and manage missing values
+print(df.isnull()) #There are no missing values in any of the columns in the dataset.
+
