46cae579b37f993b09f8fd74282f20e1c1d3b631
machh07 <marieangechhuon@gmail.com>
2025-11-08 21:08:55 -0500

    Write header + import modules and dataset
    Initial commit consisting of a header with course name, teacher's name and contributors' names. Modules and dataset also imported.

---
+++ deliverable_3.py
@@ -0,0 +1,10 @@
+#Programming in Science (420-SN1-RE)
+#Presented to: Mr. Tiago Bortoletto Vaz
+#Coded by: Marie-Ange Chhuon, Neshama Gozlan, Carlyn Matar
+
+#Import modules
+import pandas as pd
+import seaborn as sns
+
+#Import dataset
+df = pd.read_csv("Medicaldataset.csv")
