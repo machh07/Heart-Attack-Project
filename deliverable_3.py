720859f79b9fc9a520684ca77ba09a08e6021f3b
Neshama g <gozlanneshama@gmail.com>
2025-11-09 00:01:32 -0500

    Univariate non-graphical EDA
    provided the  the mean, median, mode, standard deviation,
    variance, skewness, kurtosis and quartiles  for numerical variables and the frequency counts, proportion, mode  for categorical variables (need to recheck for proportion part*)

--- deliverable_3.py
+++ deliverable_3.py
@@ -18,3 +18,22 @@ print(df.duplicated().value_counts()) #There are no duplicates in this dataset
 #Identify and manage missing values
 print(df.isnull()) #There are no missing values in any of the columns in the dataset.
 
+# Univariate non-graphical EDA
+#numerical values
+numerical_variables =["Age", "Heart rate","Systolic blood pressure" ,"Diastolic blood pressure" , "Blood sugar","CK-MB", "Troponin"]
+for num in numerical_variables:
+    print("Mean:", df[num].mean())
+    print("Median:", df[num].median())
+    print("Mode:", df[num].mode())
+    print("Variance:", df[num].var())
+    print("Skewness:", df[num].skew())
+    print("Kurtosis:", df[num].kurt())
+    print("Quartiles:", df[num].quantile([0.25, 0.5, 0.75]))
+#categorical variables
+categorical_variables = ["Gender","Result"]
+for cat in categorical_variables:
+
+    print("Frequency counts:", df[cat].value_counts())
+    print("Proportions:", df[cat].value_counts())   # ask for proportion**
+    print("Mode:", df[cat].mode())
+    print("Number of unique categories:", df[cat].nunique())
