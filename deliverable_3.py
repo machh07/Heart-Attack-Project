d881e7cb78becfc328952069efe232b38d23f5c7
machh07 <marieangechhuon@gmail.com>
2025-11-13 11:53:51 -0500

    add nunique
    i added the nunique function for part 2

--- deliverable_3.py
+++ deliverable_3.py
@@ -9,12 +9,54 @@ import seaborn as sns
 #Import dataset
 df = pd.read_csv("Medicaldataset.csv")
 
-#Preliminary steps
+# 2 Preliminary steps
 #Initial data inspection
 print(df.info())
 print(df.describe())
+print(df.nunique())
 #Handle duplicate entries
 print(df.duplicated().value_counts()) #There are no duplicates in this dataset 
 #Identify and manage missing values
 print(df.isnull()) #There are no missing values in any of the columns in the dataset.
 
+# 3 Univariate non-graphical EDA
+#numerical values
+numerical_variables =["Age", "Heart rate","Systolic blood pressure" ,"Diastolic blood pressure" , "Blood sugar","CK-MB", "Troponin"]
+for num in numerical_variables:
+    print("Mean:", df[num].mean())
+    print("Median:", df[num].median())
+    print("Mode:", df[num].mode())
+    print("Variance:", df[num].var())
+    print("Skewness:", df[num].skew())
+    print("Kurtosis:", df[num].kurt())
+    print("Quartiles:", df[num].quantile([0.25, 0.5, 0.75]))
+#categorical variables
+categorical_variables = ["Gender","Result"]
+for cat in categorical_variables:
+    print("Frequency counts:", df[cat].value_counts())
+    print("Proportions:", df[cat].value_counts(normalize=True)*100)
+    print("Mode:", df[cat].mode())
+    print("Number of unique categories:", df[cat].nunique())
+#4 Univariate graphical EDA 
+
+for num in numerical_variables:
+    
+ sns.displot(df, x= num ,bins=20)
+ sns.displot(df, x=num ,hue= "Gender") 
+ sns.displot(df, x= num , hue ="Gender" , multiple="stack")
+ sns.displot(df, x= num , hue ="Gender" , multiple="dodge") 
+ sns.displot(df, x= num , hue = "Result" , stat= "density" , common_norm=False) 
+ sns.displot(df, x= num, kind="kde", bw_adjust = 4) 
+ sns.displot(df, x=num ,hue= "Gender", kind="ecdf")
+ 
+ #Multivariate non-graphical EDA
+ #A
+ print(pd.crosstab(df["Gender"],df["Result"])) #We can observe that males (represented by the number 1) are more susceptible to heart attacks than females (represented by the number 0)
+ #Since we only have 2 categorical variables, this is the only outcome possible for a crosstab.
+ #B (Normalize with columns)
+ print(pd.crosstab(df["Gender"],df["Result"], normalize = "columns"))
+ #Once again, we observe a higher proportion of males suffering from heart attacks than females.
+ #C
+ #With only 2 categorical variables in our dataset, we are unable to generate a three-way frequency table.
+
+            
